349d21765dcb34189e65812a6e6e1d2dbe3c9657
HDreikorn <hillary@dreikorn.com>
2021-07-19 08:47:42 -0500

    added styled button

---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+export default function Button({ label, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 rounded hover:opacity-60 px-4 py-2 focus:outline-none"
+    >
+      {label}
+    </button>
+  );
+}
